64c4820eceac2097c7cdb843229040bd6808ed1f
Ayush-desaii <110298454+Ayush-desaii@users.noreply.github.com>
2024-02-27 19:27:37 +0530

    search bar component logic

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -33,10 +33,11 @@ export const useHomeFetch = () => {
     setLoading(false);
   };
 
-  // Initial render
+  // Initial & Search render
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error, setSearchTerm };
+  return { state, loading, error, searchTerm, setSearchTerm };
 };
